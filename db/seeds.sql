2000bdefa9a35ff3f64164dad2d3e855df06e9b1
Randamus <rcviets@gmail.com>
2019-11-17 17:30:08 -0700

    created/populated database

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Sunrise Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Southwest Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Classic Burger', true);
