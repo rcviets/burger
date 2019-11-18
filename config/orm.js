a6f7dc04d9cca0cb05de54efc6a03f3bd17850b7
Randamus <rcviets@gmail.com>
2019-11-17 18:28:47 -0700

    ORM creation methods/helper functions

---
+++ config/orm.js
@@ -0,0 +1,83 @@
+//Import MySQL connection
+var connection = require('../config/connection.js');
+
+//Helper Function for SQL Syntax ??
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push('?');
+    }
+    return arr.toString();
+}
+
+//Helper Function for SQL Sytanx key/value pairs
+function objColVals(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+
+        if(Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + '=' + value);
+        }
+    }
+    return arr.toString();
+}
+
+//ORM Object
+var orm = {
+
+    //Select All Function
+    selectAll: function(tableInput, cb) {
+        var queryString = 'SELECT * FROM ' + tableInput + ';';
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });    
+    },
+
+    //Create Function
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = 'INSERT INTO ' + table;
+
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        })
+    },
+
+    //Update Function
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = 'UPDATE ' + table;
+
+        queryString += 'SET ';
+        queryString += objToSql(objColVals);
+        queryString += 'WHERE ';
+        queryString += condition;
+
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+//Export ORM
+module.exports = orm;
