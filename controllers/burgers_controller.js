ed507657ebdc0d835355b492e413cad935df8e70
Randamus <rcviets@gmail.com>
2019-11-19 20:11:21 -0700

    controller file route logic created

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,43 @@
+//Dependencies
+var express = require('express');
+
+var router = express.Router();
+
+//Import Model
+var burger = require('.../models/cat.js');
+
+//Create Routes
+router.get('/', function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render('index', hbsObject);
+    });
+});
+
+router.post('/api/burgers', function(req, res) {
+    burger.create(['name', 'devoured'], [req.body.name, req.body.devoured], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put('/api/burgers/:id', function(req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    cat.update(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+});
+
+//Export Routes
+module.exports = router;
