bca578ece9c8612b77ba2b31f03289db5d85b2d1
Randamus <rcviets@gmail.com>
2019-11-19 21:19:53 -0700

    added default of false to devoured bool

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(65) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN DEFAULT 0,
     PRIMARY KEY (id)
 );
