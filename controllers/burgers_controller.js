070e9459117f7b63affdf2777d0f80b68fde6321
Randamus <rcviets@gmail.com>
2019-11-20 17:18:50 -0700

    fixed updateOne function params

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,11 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 //Import Model
-var burger = require('../models/burger.js');
+var burgers = require('../models/burger.js');
 
 //Create Routes
 router.get('/', function(req, res) {
-    burger.all(function(data) {
+    burgers.selectAll(function(data) {
         var hbsObject = {
             burgers: data
         };
@@ -17,26 +17,19 @@ router.get('/', function(req, res) {
 });
 
 router.post('/api/burgers', function(req, res) {
-    burger.create(['name', 'devoured'], [req.body.name, req.body.devoured], function(result) {
+    burgers.insertOne(['burger_name'], [req.body.name], function(result) {
         res.json({ id: result.insertId });
     });
 });
 
 router.put('/api/burgers/:id', function(req, res) {
-    var condition = 'id = ' + req.params.id;
-
-    burger.update(
-        {
-            devoured: req.body.devoured
-        },
-        condition,
-        function(result) {
-            if (result.changedRows === 0) {
-                return res.status(404).end();
-            }
-            res.status(200).end();
+    burger.updateOne(['devoured'], [req.body.devour], [req.params.id], function(result) {
+        
+        if (result.changedRows === 0) {
+            return res.status(404).end();
         }
-    );
+        res.status(200).end();
+    });
 });
 
 //Export Routes
